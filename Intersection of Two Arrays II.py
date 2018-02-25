45c53435b3ced9af29e9e8d31a139b1d206b975c
hecter <hecter654321@gmail.com>
2018-02-25 19:06:49 +0800

    pass testcase: testSample by using sort

--- Intersection of Two Arrays II.py
+++ Intersection of Two Arrays II.py
@@ -6,12 +6,28 @@ class unitest(unittest.TestCase):
         nums2 = []
         Output = []
         self.assertEqual(Solution().intersect(nums1,nums2),Output)
+    def testSample(self):
+        nums1 = [1, 2, 2, 1]
+        nums2 = [2, 2, 2]
+        Output = [2, 2]
+        self.assertEqual(Solution().intersect(nums1,nums2),Output)
 
 class Solution():
     def intersect(self, nums1, nums2):
-        if nums1 == [] and nums2 == []:
-            return []
-        return (nums2 in nums1)
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        List = []
+        while(i < len(nums1) and j < len(nums2)):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                List.append(nums1[i])
+                i += 1
+                j += 1
+        return List
 
 if __name__ == '__main__':
     unittest.main()
